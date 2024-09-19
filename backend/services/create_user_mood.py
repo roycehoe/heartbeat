616f027b4979127c86a21b931eaf1874b3932002
roycehoe <royceinbox@hotmail.com>
2024-09-19 09:36:45 +0800

    feat: deny recording mood if mood is already recorded

--- backend/services/create_user_mood.py
+++ backend/services/create_user_mood.py
@@ -35,11 +35,25 @@ def _should_alert_caregiver(user_id: int, sent_mood: SelectedMood, db: Session) 
         raise DBCreateAccountException
 
 
+def _can_record_mood(user_id: int, db: Session) -> bool:
+    try:
+        return CRUDUser(db).get(user_id).can_record_mood
+    except DBCreateAccountException:
+        raise DBCreateAccountException
+    except DBCreateAccountWithEmailAlreadyExistsException:
+        raise DBCreateAccountException
+    except Exception:
+        raise DBCreateAccountException
+
+
 def get_create_user_mood_response(
     request: MoodRequest, token: str, db: Session
 ) -> None:
     try:
         user_id = get_user_id(token)
+        if not _can_record_mood(user_id, db):
+            raise DBCreateAccountException
+
         mood_in_model = MoodIn(mood=request.mood, user_id=user_id)
         db_mood_model = Mood(
             user_id=mood_in_model.user_id,
@@ -48,8 +62,10 @@ def get_create_user_mood_response(
         )
         CRUDMood(db).create(db_mood_model)
         CRUDUser(db).update(user_id, "can_record_mood", False)
+
         if _should_alert_caregiver(user_id, mood_in_model.mood, db):
             print("ALERT CAREGIVER!!!!!")  # TODO: Implement caregiver alert here
+
     except DBCreateAccountException:
         raise DBCreateAccountException
     except DBCreateAccountWithEmailAlreadyExistsException:
