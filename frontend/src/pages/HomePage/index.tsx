ba913ac195642891c4846112e18386b5121c8823
roycehoe <royceinbox@hotmail.com>
2024-09-20 11:53:54 +0800

    feat: create css template for user landing page

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,32 +1,65 @@
-import { useState } from "react";
-import useCalendarForm from "../../hooks/useCalendarForm";
-import CalendarForm from "./CalendarForm";
-import PhotoUpload from "./PhotoUpload";
-import QRCodeResult from "./QRCodeResult";
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
 
-const HomePage = () => {
-  const useCalendarFormHook = useCalendarForm();
-  const [isShowQRCodeResult, setIsShowQRCodeResult] = useState(true);
-
-  if (isShowQRCodeResult) {
-    return (
-      <QRCodeResult
-        setIsShowQRCodeResult={setIsShowQRCodeResult}
-        resetCalendarForm={useCalendarFormHook.resetCalendarForm}
-      ></QRCodeResult>
-    );
-  }
+function HomePage() {
   return (
-    <div>
-      <PhotoUpload
-        updateCalendarForm={useCalendarFormHook.updateCalendarForm}
-      ></PhotoUpload>
-      <CalendarForm
-        useCalendarFormHook={useCalendarFormHook}
-        setIsShowQRCodeResult={setIsShowQRCodeResult}
-      ></CalendarForm>
-    </div>
+    <Box
+      bg="tomato"
+      border="24px solid"
+      borderColor="white"
+      width="100vw"
+      height="100vh"
+      color="white"
+    >
+      <Flex
+        gap="24px"
+        bg="slate.200"
+        justifyContent="space-evenly"
+        flexDirection="column"
+        height="100%"
+      >
+        <Box bg="brand.primary.300" className="brand-logo">
+          <Flex justifyContent="center">
+            <Box bg="brand.primary.200">
+              <p>Our Logo</p>
+            </Box>
+          </Flex>
+        </Box>
+
+        <Box bg="green.100" className="dashboard" flex="1">
+          <Flex width="100%" height="100%">
+            <Grid
+              templateAreas={`"plant calendar calendar"
+                              "plant calendar calendar"
+                              "plant calendar calendar"
+                              "plant calendar calendar"
+                              "happy-btn ok-btn sad-btn"`}
+              gridTemplateColumns="auto"
+              gridTemplateRows="auto"
+              gap="24px"
+              bg="brand.primary.200"
+              flex="1"
+            >
+              <GridItem area="plant" bg="red.200" m={4}>
+                Plant
+              </GridItem>
+              <GridItem area="calendar" bg="red.200" m={4}>
+                Calendar
+              </GridItem>
+              <GridItem area="happy-btn" bg="red.200" m={4}>
+                Happy
+              </GridItem>
+              <GridItem area="ok-btn" bg="red.200" m={4}>
+                Ok
+              </GridItem>
+              <GridItem area="sad-btn" bg="red.200" m={4}>
+                Sad
+              </GridItem>
+            </Grid>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
   );
-};
+}
 
 export default HomePage;
