fb4e3c336c3fc277a25cbdcff8cbd9828153398f
roycehoe <royceinbox@hotmail.com>
2024-09-23 11:57:59 +0800

    chore: add heartbeat and bfg logo

--- frontend/src/pages/TestPage.tsx
+++ frontend/src/pages/TestPage.tsx
@@ -1,5 +1,15 @@
-import { Box } from "@chakra-ui/react";
-import { BxLoader } from "@opengovsg/design-system-react";
+import {
+  Box,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { Button, BxLoader } from "@opengovsg/design-system-react";
 import { useEffect, useState } from "react";
 import { MOCK_DASHBOARD_API_RESPONSE } from "../api/constants";
 import {
@@ -12,10 +22,45 @@ import CalendarDateRange from "../components/CalendarDateRange";
 import MoodBtn from "../components/MoodBtn";
 
 const MoodBtnsProps = [
+  {
+    icon: <img src="/src/assets/logo-heartbeat.svg"></img>,
+    value: MoodValue.HAPPY,
+  },
+  { icon: <img src="/src/assets/logo-bfg.svg"></img>, value: MoodValue.HAPPY },
   { icon: <img src="/src/assets/happy.svg"></img>, value: MoodValue.HAPPY },
   { icon: <img src="/src/assets/ok.svg"></img>, value: MoodValue.OK },
   { icon: <img src="/src/assets/sad.svg"></img>, value: MoodValue.SAD },
 ];
+
+function DashboardModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+            impedit quisquam quis, aspernatur laboriosam nemo, maxime
+            accusantium voluptates cupiditate ducimus ipsa doloremque doloribus
+            atque et natus placeat nihil praesentium assumenda?
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 function HomePage() {
   const [dashboardData, setDashboardData] = useState({} as DashboardResponse);
@@ -42,6 +87,7 @@ function HomePage() {
 
   return (
     <Box width="100vw" height="100vh" display="flex" flexDirection="column">
+      <DashboardModal></DashboardModal>
       <CalendarDateRange
         dateRange={dashboardData.moods.map((mood) => {
           return new Date(mood.created_at);
