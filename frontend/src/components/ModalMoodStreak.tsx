a96516a7aacc172ac2eb458243750075450ddc87
roycehoe <royceinbox@hotmail.com>
2024-09-24 18:41:32 +0800

    feat: create separate modalMoodStreak component for modularity

---
+++ frontend/src/components/ModalMoodStreak.tsx
@@ -0,0 +1,92 @@
+import {
+  Box,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalOverlay,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+
+function ModalMoodStreak({
+  isOpen,
+  handleClose,
+  daysOfWeek,
+  tickData,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  handleClose: () => void;
+  daysOfWeek: string[];
+  tickData: boolean[];
+}) {
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalBody
+          display="flex"
+          flexDirection="column"
+          justifyContent="space-evenly"
+          bg="brand.primary.200"
+          alignItems="center"
+        >
+          <Box height="300px">Some image goes here. It is big</Box>
+          <Box>
+            <Text
+              fontWeight="600"
+              fontSize="86px"
+              color="#25AC51"
+              lineHeight="1"
+            >
+              2
+            </Text>
+          </Box>
+          <Box>
+            <Text fontWeight="600" fontSize="32px" color="#25AC51">
+              day streak!
+            </Text>
+          </Box>
+
+          <Box>
+            <Table variant="unstyled" size="sm">
+              <Thead>
+                <Tr>
+                  {daysOfWeek.map((day, index) => (
+                    <Th key={index} textAlign="center">
+                      {day}
+                    </Th>
+                  ))}
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  {tickData.map((hasTick, index) => (
+                    <Td key={index} textAlign="center">
+                      {hasTick ? (
+                        <img src="/src/assets/checkbox.svg"></img>
+                      ) : (
+                        ""
+                      )}
+                    </Td>
+                  ))}
+                </Tr>
+              </Tbody>
+            </Table>
+          </Box>
+
+          <Box marginBottom="18px">
+            Keep your streak going by checking in tomorrow
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default ModalMoodStreak;
