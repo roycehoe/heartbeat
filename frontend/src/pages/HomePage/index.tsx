c554518e919aa3e9a5aba8bdb797004fff61b531
roycehoe <royceinbox@hotmail.com>
2024-09-24 21:51:06 +0800

    feat: create loading stage and fade in after loading of data is over

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,102 +1,24 @@
-// import { Box } from "@chakra-ui/react";
-// import moment from "moment";
-// import { useEffect, useState } from "react";
-// import {
-//   DEFAULT_USER_CREDENTIALS,
-//   MOCK_DASHBOARD_API_RESPONSE,
-// } from "../../api/constants";
-// import { getUserLoginResponse, LoginRequest } from "../../api/user";
-// import Display from "./Display";
-// import MoodBtns from "./MoodBtns";
-
-// function HomePage() {
-//   const [currentIndex, setCurrentIndex] = useState<number>(0);
-
-//   // Function to log in a user with a specific LoginRequest
-//   async function login(loginRequest: LoginRequest): Promise<void> {
-//     const loginResponse = await getUserLoginResponse(loginRequest);
-//     localStorage.removeItem("token");
-//     localStorage.setItem("token", loginResponse.access_token);
-//   }
-
-//   // On component mount, retrieve index from sessionStorage
-//   useEffect(() => {
-//     const storedIndex = sessionStorage.getItem("currentIndex");
-//     if (storedIndex !== null) {
-//       setCurrentIndex(Number(storedIndex)); // Use the stored index
-//     }
-//   }, []);
-
-//   // When currentIndex changes, log in and store the index in sessionStorage
-//   useEffect(() => {
-//     if (DEFAULT_USER_CREDENTIALS.length > 0) {
-//       sessionStorage.setItem("currentIndex", currentIndex.toString()); // Store current index
-//       login(DEFAULT_USER_CREDENTIALS[currentIndex]); // Log in the user
-//     }
-//   }, [currentIndex]);
-
-//   // Function to increment the index and loop through the credentials
-//   const incrementIndex = () => {
-//     setCurrentIndex((prevIndex) =>
-//       prevIndex === DEFAULT_USER_CREDENTIALS.length - 1 ? 0 : prevIndex + 1
-//     );
-//   };
-
-//   return (
-//     <Box
-//       width="100vw"
-//       height="100vh"
-//       display="flex"
-//       flexDirection="column"
-//       bg="brand.primary.400"
-//       className="page"
-//     >
-//       <Box
-//         display="flex"
-//         flexDirection="column"
-//         bg="brand.secondary.400"
-//         marginX="56px"
-//         marginTop="68px"
-//         marginBottom="32px"
-//         gap="60px"
-//         justifyContent="space-between"
-//         height="100%"
-//         className="page--group"
-//       >
-//         <Display
-//           dashboardData={MOCK_DASHBOARD_API_RESPONSE}
-//           goToNextUser={incrementIndex}
-//         ></Display>
-//         <MoodBtns
-//           isDisabled={!MOCK_DASHBOARD_API_RESPONSE.can_record_mood}
-//           moodsCreatedAt={MOCK_DASHBOARD_API_RESPONSE.moods.map((mood) =>
-//             moment(mood.created_at)
-//           )}
-//           streak={MOCK_DASHBOARD_API_RESPONSE.consecutive_checkins}
-//         ></MoodBtns>
-//       </Box>
-//     </Box>
-//   );
-// }
-
-// export default HomePage;
-import { Box } from "@chakra-ui/react";
+import { Box, Fade, Spinner } from "@chakra-ui/react";
+import { SpinnerIcon } from "@opengovsg/design-system-react";
 import moment from "moment";
 import { useEffect, useState } from "react";
+import { DEFAULT_USER_CREDENTIALS } from "../../api/constants";
 import {
-  DEFAULT_USER_CREDENTIALS,
-  MOCK_DASHBOARD_API_RESPONSE,
-} from "../../api/constants";
-import { getUserLoginResponse, LoginRequest } from "../../api/user";
+  DashboardResponse,
+  getUserDashboardResponse,
+  getUserLoginResponse,
+  LoginRequest,
+} from "../../api/user";
 import Display from "./Display";
 import MoodBtns from "./MoodBtns";
 
 function HomePage() {
   const [currentIndex, setCurrentIndex] = useState<number>(() => {
-    // Retrieve stored index from sessionStorage, or use 0 as the default
     const storedIndex = localStorage.getItem("currentIndex");
     return storedIndex !== null ? Number(storedIndex) : 0;
   });
+  const [dashboardData, setDashboardData] = useState<DashboardResponse>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   async function login(loginRequest: LoginRequest): Promise<void> {
     const loginResponse = await getUserLoginResponse(loginRequest);
@@ -104,9 +26,16 @@ function HomePage() {
   }
 
   useEffect(() => {
-    if (DEFAULT_USER_CREDENTIALS.length > 0) {
-      login(DEFAULT_USER_CREDENTIALS[currentIndex]);
-    }
+    const loadDashboard = async () => {
+      setIsLoading(true);
+      if (DEFAULT_USER_CREDENTIALS.length > 0) {
+        await login(DEFAULT_USER_CREDENTIALS[currentIndex]);
+      }
+      const dashboardResponse = await getUserDashboardResponse();
+      setDashboardData(dashboardResponse);
+      setIsLoading(false);
+    };
+    loadDashboard();
   }, [currentIndex]);
 
   const incrementIndex = () => {
@@ -118,6 +47,18 @@ function HomePage() {
     localStorage.setItem("currentIndex", nextIndex.toString());
   };
 
+  if (isLoading) {
+    return (
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        bg="brand.primary.400"
+        className="page"
+      ></Box>
+    );
+  }
   return (
     <Box
       width="100vw"
@@ -127,30 +68,32 @@ function HomePage() {
       bg="brand.primary.400"
       className="page"
     >
-      <Box
-        display="flex"
-        flexDirection="column"
-        bg="brand.secondary.400"
-        marginX="56px"
-        marginTop="68px"
-        marginBottom="32px"
-        gap="60px"
-        justifyContent="space-between"
-        height="100%"
-        className="page--group"
-      >
-        <Display
-          dashboardData={MOCK_DASHBOARD_API_RESPONSE}
-          goToNextUser={incrementIndex}
-        ></Display>
-        <MoodBtns
-          isDisabled={!MOCK_DASHBOARD_API_RESPONSE.can_record_mood}
-          moodsCreatedAt={MOCK_DASHBOARD_API_RESPONSE.moods.map((mood) =>
-            moment(mood.created_at)
-          )}
-          streak={MOCK_DASHBOARD_API_RESPONSE.consecutive_checkins}
-        ></MoodBtns>
-      </Box>
+      <Fade in={!isLoading}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          bg="brand.secondary.400"
+          marginX="56px"
+          marginTop="68px"
+          marginBottom="32px"
+          gap="60px"
+          justifyContent="space-between"
+          height="100%"
+          className="page--group"
+        >
+          <Display
+            dashboardData={dashboardData}
+            goToNextUser={incrementIndex}
+          ></Display>
+          <MoodBtns
+            isDisabled={!dashboardData.can_record_mood}
+            moodsCreatedAt={dashboardData.moods.map((mood) =>
+              moment(mood.created_at)
+            )}
+            streak={dashboardData.consecutive_checkins}
+          ></MoodBtns>
+        </Box>
+      </Fade>
     </Box>
   );
 }
