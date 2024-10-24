4384ec4a25222422fd2ef17a8651bf62808d7f9c
roycehoe <royceinbox@hotmail.com>
2024-10-24 13:49:32 +0800

    feat: create placeholder encryption functions

--- backend/utils/hashing.py
+++ backend/utils/hashing.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from passlib.context import CryptContext
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -10,3 +12,13 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_context.verify(plain_password, hashed_password)
+
+
+def encrypt_data(data: Any):
+    """Placeholder encryption function till decision on encryption is reached"""
+    return data
+
+
+def decrypt_data(data: Any):
+    """Placeholder decryption function till decision on encryption is reached"""
+    return data
