4f56b980ffa59982f64947f3218a99d62ebfb19f
roycehoe <royceinbox@hotmail.com>
2025-04-15 01:35:03 +0800

    feat: implement personal information so-called form

---
+++ frontend/src/components/FormFieldsViewUser.tsx
@@ -0,0 +1,59 @@
+import { Box } from "@chakra-ui/react";
+import { CreateUpdateUserFormFieldProps } from "../pages/Admin/constants";
+import { CreateUserForm } from "../pages/Admin/CreateUser";
+import { UpdateUserForm } from "../pages/Admin/UpdateUser";
+import FormInputUser from "./FormInputUser";
+import FormSelectUser from "./FormSelectUser";
+
+export function FormFieldsViewUser(props: {
+  createUpdateUserFormFields: Record<
+    keyof CreateUserForm | keyof UpdateUserForm,
+    CreateUpdateUserFormFieldProps
+  >;
+  createUserForm: CreateUserForm;
+  setCreateUserForm: React.Dispatch<React.SetStateAction<CreateUserForm>>;
+}) {
+  const handleChange = (e, field) => {
+    props.setCreateUserForm({
+      ...props.createUserForm,
+      [field]: e.target.value,
+    });
+  };
+
+  return (
+    <Box display="flex" flexDirection="column" gap="16px">
+      {Object.keys(props.createUpdateUserFormFields).map((field) => {
+        const { formLabel, isRequired, type, options } =
+          props.createUpdateUserFormFields[field];
+
+        if (type === "select") {
+          return (
+            <FormSelectUser
+              field={field}
+              isRequired={isRequired}
+              isDisabled={false}
+              formLabel={formLabel}
+              type={type}
+              value={props.createUserForm[field]}
+              onChange={(e) => handleChange(e, field)}
+              placeholder={formLabel}
+              options={options}
+            ></FormSelectUser>
+          );
+        }
+        return (
+          <FormInputUser
+            field={field}
+            isRequired={isRequired}
+            isDisabled={false}
+            formLabel={formLabel}
+            type={type}
+            value={props.createUserForm[field]}
+            onChange={(e) => handleChange(e, field)}
+            placeholder={formLabel}
+          ></FormInputUser>
+        );
+      })}
+    </Box>
+  );
+}
