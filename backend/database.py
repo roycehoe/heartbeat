38152d3b8fd79f92502b7a323c2425699f0a725a
roycehoe <royceinbox@hotmail.com>
2025-04-19 22:29:25 +0800

    chore: implement postgresql db

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,14 @@
+from dotenv import dotenv_values
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = (
+    dotenv_values(".env").get("SQLALCHEMY_DATABASE_URL")
+    or "postgresql://postgres:password@localhost:5432/postgres"
 )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
