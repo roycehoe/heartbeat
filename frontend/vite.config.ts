e7595123b4cdbff750cdb293894ad156a43dab05
roycehoe <royceinbox@hotmail.com>
2025-08-03 13:12:13 +0800

    feat: implement pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["public/assets/logo-circle-heartbeat.png"],
+      manifest: {
+        name: "Heartbeat",
+        short_name: "Heartbeat",
+        start_url: "https://heartbeat.fancybinary.sg/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [],
+      },
+    }),
+  ],
 });
