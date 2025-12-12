9585f0d1b60b00f93e72c1c0b3e2e428e95c2715
roycehoe <royceinbox@hotmail.com>
2025-12-12 10:39:42 +0800

    feat: implement new caregiver login

---
+++ frontend/src/pages/Login.tsx/CaregiverOrCareReceipientSelection.tsx
@@ -0,0 +1,39 @@
+import { Box, Text } from "@chakra-ui/react";
+import { Button } from "@opengovsg/design-system-react";
+import { useNavigate } from "react-router-dom";
+import { LogInFormState } from "./Index";
+
+function CaregiverOrCareReceipientSelection({
+  setLogInFormState,
+}: {
+  setLogInFormState: (logInFormState: LogInFormState) => void;
+}) {
+  const navigate = useNavigate();
+  const onLogInAsCaregiverButtonClick = () => {
+    navigate("/admin");
+  };
+  const onGoBackButtonClick = () => {
+    localStorage.removeItem("token");
+    setLogInFormState(LogInFormState.CaregiverAuthenticate);
+  };
+
+  return (
+    <div>
+      <Box display="flex" flexDirection="column" gap="60px">
+        <Box display="flex" flexDirection="column" gap="12px">
+          <Button width="100%" onClick={onLogInAsCaregiverButtonClick}>
+            <Text>Log in as Caregiver</Text>
+          </Button>
+          <Button width="100%" onClick={() => console.log("caregiver")}>
+            <Text>Log in as Care Receipient</Text>
+          </Button>
+        </Box>
+        <Button width="100%" onClick={onGoBackButtonClick}>
+          <Text>Go back</Text>
+        </Button>
+      </Box>
+    </div>
+  );
+}
+
+export default CaregiverOrCareReceipientSelection;
