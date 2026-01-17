fe3d1e81caa5f7d1fd36bd07781014f95359bb23
roycehoe <royceinbox@hotmail.com>
2026-01-17 10:15:55 +0800

    chore: remove test Clerk key

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 // const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-// const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuY2FyZWNvbXBhc3Muc2ck";
-const PUBLISHABLE_KEY = "pk_test_bWFqb3ItY29yYWwtNi5jbGVyay5hY2NvdW50cy5kZXYk";
+const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuY2FyZWNvbXBhc3Muc2ck";
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
